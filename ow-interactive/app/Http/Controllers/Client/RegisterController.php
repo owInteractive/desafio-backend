d39319bf399224db9080f3a03d53411522530b71
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2021-05-22 13:42:16 -0300

    feat: create balance register for user when this is registered

--- ow-interactive/app/Http/Controllers/Client/RegisterController.php
+++ ow-interactive/app/Http/Controllers/Client/RegisterController.php
@@ -6,19 +6,25 @@ use App\Helpers\Response;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
     public function handle(RegisterRequest $request)
     {
+        DB::beginTransaction();
         try {
             $user = new User();
             $user->fill($request->all());
             $user->password = Hash::make($user->password);
             $user->save();
+            $user->financial()->create();
+
+            DB::commit();
             return Response::created(["message" => "Usuário criado com sucesso"]);
         } catch (\Throwable $th) {
+            DB::rollback();
             return Response::badRequest(["message" => "Erro ao criar usuário"]);
         }
     }
