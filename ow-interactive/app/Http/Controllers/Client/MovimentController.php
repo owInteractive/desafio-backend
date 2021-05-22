66d4c299fd21a8ba1a37e1667e7611c8da203870
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2021-05-22 13:49:24 -0300

    feat: add moviment list of user

--- ow-interactive/app/Http/Controllers/Client/MovimentController.php
+++ ow-interactive/app/Http/Controllers/Client/MovimentController.php
@@ -6,11 +6,42 @@ use App\Helpers\Response;
 use App\Http\Controllers\Controller;
 use App\Models\Financial;
 use App\Models\Moviment;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class MovimentController extends Controller
 {
+
+    public function index(Request $request)
+    {
+        try {
+            $userId = $request->get('userId');
+            $limit = $request->get('limit') ?? 20;
+
+
+            $user = User::with('financial:id,user_id,current_balance')
+                ->findOrFail($userId);
+
+            $moviments = Moviment::select(
+                'moviments.value',
+                'moviments.created_at',
+                'moviment_types.name as type'
+            )
+                ->join('moviment_types', 'moviment_types.id', 'moviments.moviment_type_id')
+                ->where('moviments.financial_id', $user->financial->id)
+                ->orderByDesc('moviments.id')
+                ->paginate($limit);
+
+            $user->financial->moviments = $moviments;
+
+            return Response::success($user);
+        } catch (\Throwable $th) {
+            throw $th;
+            return Response::serverError();
+        }
+    }
+
     public function store(Request $request)
     {
 
