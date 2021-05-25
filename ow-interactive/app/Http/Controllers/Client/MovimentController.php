50dc15763a085c5af81b9c81bb727806e9c1d751
Gabriel Scaranello <gabrieljscaranello@gmail.com>
2021-05-25 10:13:09 -0300

    refactor: move basic moviment operations to MovimentService

--- ow-interactive/app/Http/Controllers/Client/MovimentController.php
+++ ow-interactive/app/Http/Controllers/Client/MovimentController.php
@@ -9,6 +9,7 @@ use App\Models\Financial;
 use App\Models\Moviment;
 use App\Models\User;
 use App\Services\FinancialService;
+use App\Services\MovimentService;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -21,60 +22,20 @@ class MovimentController extends Controller
     {
         try {
             $userId = Auth::user()->id;
-            $limit = $request->get('limit') ?? 20;
-
-
-            $user = User::with('financial:id,user_id,current_balance')
-                ->findOrFail($userId);
-
-            $moviments = Moviment::select(
-                'moviments.id',
-                'moviments.value',
-                'moviments.created_at',
-                'moviment_types.name as type'
-            )
-                ->join('moviment_types', 'moviment_types.id', 'moviments.moviment_type_id')
-                ->where('moviments.financial_id', $user->financial->id)
-                ->orderByDesc('moviments.id')
-                ->paginate($limit);
-
-            $user->financial->moviments = $moviments;
-
+            $user = MovimentService::index($request, $userId);
             return Response::success($user);
         } catch (\Throwable $th) {
-            throw $th;
             return Response::serverError();
         }
     }
 
     public function store(Request $request)
     {
-
-        $userId = Auth::user()->id;
-        DB::beginTransaction();
-
         try {
-            $financial = Financial::where('user_id', $userId)->first();
-
-            if (!$financial) {
-                $financial = new Financial();
-                $financial->user_id = $userId;
-                $financial->save();
-            }
-
-            $financial->moviments()->create($request->all());
-
-            $financial->current_balance = $request->get('moviment_type_id') === 1 ?
-                $financial->current_balance - $request->get('value') :
-                $financial->current_balance + $request->get('value');
-
-            $financial->save();
-
-            DB::commit();
-
+            $userId = Auth::user()->id;
+            MovimentService::store($request, $userId);
             return Response::success(["message" => "Movimento registrado com sucesso."]);
         } catch (\Throwable $th) {
-            DB::rollback();
             return Response::badRequest(["message" => "Erro ao registrar movimento."]);
         }
     }
@@ -82,9 +43,9 @@ class MovimentController extends Controller
     public function destroy($id)
     {
         try {
-            $moviment = Moviment::findOrFail($id);
+            $userId = Auth::user()->id;
 
-            if ($moviment->delete()) {
+            if (MovimentService::destroy($id, $userId)) {
                 return Response::success(['message' => "Movimentação excluída com suceso."]);
             }
 
---
+++ ow-interactive/app/Services/MovimentService.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Financial;
+use App\Models\Moviment;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class MovimentService
+{
+    public static function index($request, $userId)
+    {
+        $userId = Auth::user()->id;
+        $limit = $request->get('limit') ?? 20;
+
+        $user = User::with('financial:id,user_id,current_balance')
+            ->findOrFail($userId);
+
+        $moviments = Moviment::select(
+            'moviments.id',
+            'moviments.value',
+            'moviments.created_at',
+            'moviment_types.name as type'
+        )
+            ->join('moviment_types', 'moviment_types.id', 'moviments.moviment_type_id')
+            ->where('moviments.financial_id', $user->financial->id)
+            ->orderByDesc('moviments.id')
+            ->paginate($limit);
+
+        $user->financial->moviments = $moviments;
+
+        return $user;
+    }
+
+    public static function store($request, $userId)
+    {
+        DB::beginTransaction();
+
+        try {
+            $financial = Financial::where('user_id', $userId)->first();
+
+            if (!$financial) {
+                $financial = new Financial();
+                $financial->user_id = $userId;
+                $financial->save();
+            }
+
+            $financial->moviments()->create($request->all());
+
+            $financial->current_balance = $request->get('moviment_type_id') === 1 ?
+                $financial->current_balance - $request->get('value') :
+                $financial->current_balance + $request->get('value');
+
+            $financial->save();
+
+            DB::commit();
+
+            return true;
+        } catch (\Throwable $th) {
+            DB::rollback();
+            throw $th;
+        }
+    }
+
+    public static function destroy($id, $userId)
+    {
+
+        try {
+            $moviment = Moviment::join('financials', 'financials.id', 'moviment.financial_id')
+                ->where('financials.user_id', $userId)
+                ->findOrFail($id);
+
+            return $moviment->delete();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+}
