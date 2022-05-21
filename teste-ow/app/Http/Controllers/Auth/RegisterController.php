4e112459e2b4bcdd19858e644530b68846c1b395
MatheusR1 <matheusrocha_456@hotmail.com>
2022-05-21 16:35:16 -0300

    controller para auth

---
+++ teste-ow/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\API\BaseController;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class RegisterController extends BaseController
+{
+    public function register (Request $request , User $user) 
+    {
+        $input = $request->all();
+
+        $validator = Validator::make($input, [
+            'name'     => 'required',
+            'password' => 'required',
+            'email'    => 'required'
+        ]);
+
+        if($validator->fails()){
+            return $this->sendError('fail request', $validator->errors(), 400);
+        }
+
+        if (!$user = $user->create($input)) {
+            return $this->sendError('fail create user', $validator->errors(), 500);
+        }
+
+        return $this->sendResponse($user, 'user created'); 
+    }
+}
