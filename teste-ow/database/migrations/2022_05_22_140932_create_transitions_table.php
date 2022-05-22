54465e749f15380c158c8dff3268113b65ef7161
MatheusR1 <matheusrocha_456@hotmail.com>
2022-05-22 18:37:38 -0300

    criar alguns classes para arquitera funcionar melhor e cricao de rota

---
+++ teste-ow/database/migrations/2022_05_22_140932_create_transitions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransitionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transitions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->enum('types',['debit', 'credit', 'chargeback']);
+            $table->decimal('value', 10, 3);
+            $table->foreignIdFor(User::class);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transitions');
+    }
+}
--- teste-ow/routes/api.php
+++ teste-ow/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\TransitionsController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,6 +10,9 @@ Route::post('/login', [LoginController::class, 'login']);
 Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth:sanctum'); 
 Route::post('/register', [RegisterController::class, 'register']);
 
+Route::get('/download/transitions', [TransitionsController::class, 'download']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::apiResource('users', UserController::class);
+    Route::apiResource('transitions', TransitionsController::class)->except('update');
 });
