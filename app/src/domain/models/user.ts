0a0d5e0276c592edc4bfb619670cf01b0c4d09b4
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-10-14 22:25:55 -0300

    feat: cria model User

---
+++ app/src/domain/models/user.ts
@@ -0,0 +1,10 @@
+export type User = {
+    id: number;
+    name: string;
+    email: string;
+    brithDay: string;
+    role: string;
+    password: string;
+    createdAt: string;
+    updatedAt: string;
+}
