3f37a28a7875d6ab2aab133430671f64ffda9338
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-10-14 23:18:49 -0300

    test: garante que AddUserController vai retornar serverError se loadUsersByEmail quebrar

--- app/src/presentation/controllers/users-controller/add-user-controller.test.ts
+++ app/src/presentation/controllers/users-controller/add-user-controller.test.ts
@@ -1,42 +1,64 @@
-import { describe, expect, test } from 'vitest'
-import { AddUserController } from './add-user-controller';
-import { AddUserSpy, LoadUsersByEmailSpy, mockUser } from '@/domain/tests/users.mock';
-import { badRequest } from '@/presentation/helpers/http-helper';
-import { EmailAlreadyInUseError } from '@/presentation/errors';
+import { describe, expect, test, vitest } from 'vitest'
+import { AddUserController } from './add-user-controller'
+import {
+  AddUserSpy,
+  LoadUsersByEmailSpy,
+  mockUser,
+} from '@/domain/tests/users.mock'
+import { badRequest, serverError } from '@/presentation/helpers/http-helper'
+import { EmailAlreadyInUseError } from '@/presentation/errors'
 
 describe('AddUserController', () => {
-    type SutType = {
-        sut: AddUserController
-        addUserUseCaseSpy: AddUserSpy
-        loadUsersByEmailUseCaseSpy: LoadUsersByEmailSpy
+  type SutType = {
+    sut: AddUserController
+    addUserUseCaseSpy: AddUserSpy
+    loadUsersByEmailUseCaseSpy: LoadUsersByEmailSpy
+  }
+  function makeSut(): SutType {
+    const addUserUseCaseSpy = new AddUserSpy()
+    const loadUsersByEmailUseCaseSpy = new LoadUsersByEmailSpy()
+    const sut = new AddUserController(
+      addUserUseCaseSpy,
+      loadUsersByEmailUseCaseSpy
+    )
+
+    return {
+      sut,
+      addUserUseCaseSpy,
+      loadUsersByEmailUseCaseSpy,
     }
-    function makeSut(): SutType {
-        const addUserUseCaseSpy = new AddUserSpy()
-        const loadUsersByEmailUseCaseSpy = new LoadUsersByEmailSpy()
-        const sut = new AddUserController(addUserUseCaseSpy, loadUsersByEmailUseCaseSpy)
+  }
+  describe('handle()', () => {
+    test('should call loadUsersByEmailUseCase with the correct email', async () => {
+      const { sut, loadUsersByEmailUseCaseSpy } = makeSut()
+      const mockedUser = mockUser()
+      await sut.handle(mockedUser)
 
-        return {
-            sut,
-            addUserUseCaseSpy,
-            loadUsersByEmailUseCaseSpy
-        }
-    }
-    describe('handle()', () => {
-        test('should call loadUsersByEmailUseCase with the correct email', async () => {
-            const { sut, loadUsersByEmailUseCaseSpy } = makeSut()
-            const mockedUser = mockUser()
-            await sut.handle(mockedUser)
+      expect(loadUsersByEmailUseCaseSpy.loadByEmailParams).toEqual({
+        email: mockedUser.email,
+      })
+    })
 
-            expect(loadUsersByEmailUseCaseSpy.loadByEmailParams).toEqual({email:mockedUser.email})
-        });
+    test('should return EmailAlreadyInUseError if already exists an user with this email', async () => {
+      const { sut, loadUsersByEmailUseCaseSpy } = makeSut()
+      const mockedUser = mockUser()
+      loadUsersByEmailUseCaseSpy.loadByEmailResult = mockedUser
+      const response = await sut.handle(mockedUser)
 
-        test('should return EmailAlreadyInUseError if already exists an user with this email', async () => {
-            const { sut, loadUsersByEmailUseCaseSpy } = makeSut()
-            const mockedUser = mockUser()
-            loadUsersByEmailUseCaseSpy.loadByEmailResult = mockedUser
-            const response = await sut.handle(mockedUser)
+      expect(response).toEqual(
+        badRequest(new EmailAlreadyInUseError(mockedUser.email))
+      )
+    })
 
-            expect(response).toEqual(badRequest(new EmailAlreadyInUseError(mockedUser.email)))
-        });
+    test('should return serverError if loadUsersByEmail throws', async () => {
+        const { sut, loadUsersByEmailUseCaseSpy } = makeSut()
+        const mockedUser = mockUser()
+        const mockedError = new Error('some_error')
+        vitest.spyOn(loadUsersByEmailUseCaseSpy, 'loadByEmail').mockRejectedValueOnce(mockedError)
+        const response = await sut.handle(mockedUser)
+        expect(response).toEqual(
+          serverError(mockedError)
+        )
     });
-});
+  })
+})
