ceef26cf890ac98ddcd32c73b1aa39b3dfbb6d14
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-10-14 23:26:25 -0300

    feat: garante que addUserUseCase será chamado corretamente

--- app/src/presentation/controllers/users-controller/add-user-controller.ts
+++ app/src/presentation/controllers/users-controller/add-user-controller.ts
@@ -5,22 +5,29 @@ import { badRequest, serverError } from '@/presentation/helpers/http-helper'
 import { Controller, HttpResponse } from '@/presentation/protocols'
 
 export class AddUserController implements Controller {
-  constructor(private readonly addUserUseCase: AddUser, private readonly loadUsersByEmailUseCase: LoadUsersByEmail) {}
+  constructor(
+    private readonly addUserUseCase: AddUser,
+    private readonly loadUsersByEmailUseCase: LoadUsersByEmail
+  ) {}
 
   async handle(
     request: AddUserController.Request
   ): Promise<AddUserController.Response> {
-   try {
-    const emaiInUse = await this.loadUsersByEmailUseCase.loadByEmail({ email: request.email })
-    
-    if (emaiInUse) {
-        return badRequest(new EmailAlreadyInUseError(request.email))
-    }
+    try {
+      const emaiInUse = await this.loadUsersByEmailUseCase.loadByEmail({
+        email: request.email,
+      })
 
-    return null
-   } catch (error) {
-    return serverError(error)
-   }
+      if (emaiInUse) {
+        return badRequest(new EmailAlreadyInUseError(request.email))
+      }
+
+      await this.addUserUseCase.add(request)
+
+      return null
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
 
