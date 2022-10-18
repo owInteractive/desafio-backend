892e3472235a1198ac24a1271ed216fd1901b7bb
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-10-18 18:57:32 -0300

    feat: adiciona campo para transações do usuário

--- app/src/domain/models/user.ts
+++ app/src/domain/models/user.ts
@@ -1,10 +1,13 @@
+import { Transaction } from "./transactions";
+
 export type User = {
     id?: number;
     name: string;
     email: string;
     birthDay: Date | string;
-    role?: string;
+    role?: string
     password: string
+    transactions?: Transaction[]
     createdAt?: Date;
     updatedAt?: Date;
 }
