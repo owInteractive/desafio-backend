9a655261c266736ed36ed39c4922ee134b2489ca
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-10-17 21:33:03 -0300

    test: implementa GET /users

--- app/src/main/routes/users-routes.spec.ts
+++ app/src/main/routes/users-routes.spec.ts
@@ -1,4 +1,5 @@
 import { mockAddUser } from '@/domain/tests/users-mock';
+import UsersSequelize from '@/infra/database/sequelize/models/Users';
 import { migrate, truncate } from '@/infra/database/sequelize/__tests__/utils';
 import request from 'supertest'
 import { beforeAll, beforeEach, describe, expect, test } from 'vitest'
@@ -24,6 +25,20 @@ describe('Users Routes', () => {
             expect(res.body.name).toBe(body.name)
             expect(res.body.email).toBe(body.email)
             expect(res.body.id).toBeTruthy()
+        });
+    });
+
+    describe('GET /users', () => {
+        test('should return 200 with valid users in descending order', async () => {
+            
+            const [userOne, userTwo] = await UsersSequelize.bulkCreate([mockAddUser(),mockAddUser()])
+            
+            const res = await request(app)
+                .get('/users')
+                .expect(200)
+            
+            expect(res.body[0].id).toBe(userOne.getDataValue('id'))
+            expect(res.body[1].id).toBe(userTwo.getDataValue('id'))
         });
     });
 });
