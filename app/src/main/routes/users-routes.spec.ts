19e1f5c210c9294e4a09bcd836bf12ba0de4e319
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-10-17 21:43:55 -0300

    test: garante que vai ordernar de forma ascendente se requisitado

--- app/src/main/routes/users-routes.spec.ts
+++ app/src/main/routes/users-routes.spec.ts
@@ -1,44 +1,66 @@
-import { mockAddUser } from '@/domain/tests/users-mock';
-import UsersSequelize from '@/infra/database/sequelize/models/Users';
-import { migrate, truncate } from '@/infra/database/sequelize/__tests__/utils';
+import { mockAddUser } from '@/domain/tests/users-mock'
+import UsersSequelize from '@/infra/database/sequelize/models/Users'
+import { migrate, truncate } from '@/infra/database/sequelize/__tests__/utils'
 import request from 'supertest'
-import { beforeAll, beforeEach, describe, expect, test } from 'vitest'
+import { afterAll, beforeAll, beforeEach, describe, expect, test } from 'vitest'
 import app from '../config/app'
 
 describe('Users Routes', () => {
-    describe('POST /users', () => {
-        beforeAll(async () => {
-            await migrate()
-        })
+  beforeAll(async () => {
+    await migrate()
+  })
 
-        beforeEach(async () => {
-            await truncate()
-        })
-        test('should return 200 with a valid user', async () => {
-            const body = mockAddUser();
-            
-            const res = await request(app)
-                .post('/users')
-                .send(body)
-                .expect(200)
+  beforeEach(async () => {
+    await truncate()
+  })
 
-            expect(res.body.name).toBe(body.name)
-            expect(res.body.email).toBe(body.email)
-            expect(res.body.id).toBeTruthy()
-        });
-    });
+  afterAll(async () => {
+    await truncate()
+  })
+  describe('POST /users', () => {
+    test('should return 200 with a valid user', async () => {
+      const body = mockAddUser()
 
-    describe('GET /users', () => {
-        test('should return 200 with valid users in descending order', async () => {
-            
-            const [userOne, userTwo] = await UsersSequelize.bulkCreate([mockAddUser(),mockAddUser()])
-            
-            const res = await request(app)
-                .get('/users')
-                .expect(200)
-            
-            expect(res.body[0].id).toBe(userOne.getDataValue('id'))
-            expect(res.body[1].id).toBe(userTwo.getDataValue('id'))
-        });
-    });
-});
+      const res = await request(app).post('/users').send(body).expect(200)
+
+      expect(res.body.name).toBe(body.name)
+      expect(res.body.email).toBe(body.email)
+      expect(res.body.id).toBeTruthy()
+    })
+  })
+
+  describe('GET /users', () => {
+    test('should return 200 with valid users in descending order', async () => {
+      const now = new Date()
+      const older = new Date(now.getTime() - 1000)
+
+      const [userOne, userTwo] = await UsersSequelize.bulkCreate([
+        { ...mockAddUser(), createdAt: now },
+        { ...mockAddUser(), createdAt: older },
+      ])
+
+      const res = await request(app).get('/users').expect(200)
+
+      expect(res.body[0].id).toBe(userOne.getDataValue('id'))
+      expect(res.body[1].id).toBe(userTwo.getDataValue('id'))
+    })
+
+    test('should return 200 with valid users in asccending order', async () => {
+      const now = new Date()
+      const older = new Date(now.getTime() - 1000)
+
+      const [userOne, userTwo] = await UsersSequelize.bulkCreate([
+        { ...mockAddUser(), createdAt: now },
+        { ...mockAddUser(), createdAt: older },
+      ])
+
+      const res = await request(app)
+        .get('/users')
+        .query({ order: 'asc' })
+        .expect(200)
+
+      expect(res.body[0].id).toBe(userTwo.getDataValue('id'))
+      expect(res.body[1].id).toBe(userOne.getDataValue('id'))
+    })
+  })
+})
