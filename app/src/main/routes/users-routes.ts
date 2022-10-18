74649e35487e597642b14a1bd87ff949ad5af2e6
Guilherme Teixeira Ais <guilherme.teixeira@ctctech.com.br>
2022-10-17 21:32:54 -0300

    feat: implementa GET /users

--- app/src/main/routes/users-routes.ts
+++ app/src/main/routes/users-routes.ts
@@ -2,7 +2,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapters/express/express-route-adapter'
 import { makeAddUserController } from '../factories/controllers/user/add-user-controller-factory'
+import { makeLoadUsersController } from '../factories/controllers/user/load-users-controller-factory'
 
 export const usersRoutes = (router: Router): void => {
+  router.get('/users', adaptRoute(makeLoadUsersController()))
   router.post('/users', adaptRoute(makeAddUserController()))
 }
--- app/src/presentation/controllers/users-controller/index.ts
+++ app/src/presentation/controllers/users-controller/index.ts
@@ -1,1 +1,2 @@
 export * from './add-user-controller'
+export * from './load-users-controller'
