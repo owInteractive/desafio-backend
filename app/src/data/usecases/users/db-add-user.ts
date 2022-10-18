cb5abab19eb2ebd7ef4fcac94ac8bec3b75e10a3
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-10-18 15:45:33 -0300

    feat: garante que DbAddUser irá chamar dateFormatter com os parametros corretos

--- app/src/data/usecases/users/db-add-user.ts
+++ app/src/data/usecases/users/db-add-user.ts
@@ -1,16 +1,15 @@
 import { AddUserRepository } from '@/data/protocols/database/users/add-user-repository';
+import { DateFormatter } from '@/data/protocols/utils';
 import { AddUser } from '@/domain/usecases/users';
 
 export class DbAddUser implements AddUser {
     constructor(
         private readonly addUserRepository: AddUserRepository,
+        private readonly dateFormatter: DateFormatter
     ) {}
 
-    async add(params: AddUser.Params): Promise<AddUser.Result> {
-        const user: AddUser.Params = {
-            ...params,
-            birthDay: new Date(params.birthDay)
-        }
+    async add(user: AddUser.Params): Promise<AddUser.Result> {
+        user.birthDay = this.dateFormatter.format(user.birthDay)
         const newUser = await this.addUserRepository.add(user);
         return newUser
     }
