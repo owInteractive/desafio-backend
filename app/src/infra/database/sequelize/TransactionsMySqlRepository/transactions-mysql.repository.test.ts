264c9f375507afe3ef3cc79f76ad03ef2797de71
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-10-19 00:12:15 -0300

    test: garante que chargeback ira funcionar

--- app/src/infra/database/sequelize/TransactionsMySqlRepository/transactions-mysql.repository.test.ts
+++ app/src/infra/database/sequelize/TransactionsMySqlRepository/transactions-mysql.repository.test.ts
@@ -1,7 +1,8 @@
-import { User } from '@/domain/models'
+import { Transaction, User } from '@/domain/models'
 import { mockAddTransaction } from '@/domain/tests/mock-transactions'
 import { mockAddUser } from '@/domain/tests/mock-users'
 import { afterAll, beforeAll, beforeEach, describe, expect, test } from 'vitest'
+import TransactionSequelize from '../models/Transaction'
 import UsersSequelize from '../models/User'
 import { migrate, truncate } from '../__tests__/utils'
 import { TransactionsMySqlReposiory } from './transactions-mysql.repository'
@@ -14,6 +15,39 @@ describe('TransactionsMySqlRepository', () => {
   function makeUser(): Promise<User> {
     const user = mockAddUser()
     return UsersSequelize.create(user) as any
+  }
+
+  async function makeTransaction(): Promise<Transaction> {
+    const transaction = mockAddTransaction()
+    const to = await makeUser()
+    const from = await makeUser()
+    const rawTransactionDb = (
+      await TransactionSequelize.create({
+        ...transaction,
+        from: from.id,
+        to: to.id,
+        chargebackFrom: transaction.chargebackFrom?.id,
+      })
+    ).toJSON()
+
+    const { From, ChargebackFrom, To, ...transactionDb } = (
+      await TransactionSequelize.findOne({
+        where: {
+          id: rawTransactionDb.id,
+        },
+        include: {
+          all: true,
+        },
+      })
+    ).toJSON()
+    const transactionMapped = {
+      ...transactionDb,
+      from: From,
+      to: To,
+      chargebackFrom: ChargebackFrom,
+    }
+
+    return transactionMapped
   }
 
   beforeAll(async () => {
@@ -43,7 +77,39 @@ describe('TransactionsMySqlRepository', () => {
       expect(transaction.type).toBe(mockedTransaction.type)
       expect(transaction.to.id).toEqual(to.id)
       expect(transaction.from.id).toEqual(from.id)
+
+      const transactionExists = await TransactionSequelize.findByPk(
+        transaction.id
+      )
+      expect(transactionExists).toBeTruthy()
+      expect(transactionExists.toJSON().id).toBe(transaction.id)
+    })
+
+    test('should add and return an transaction on success ', async () => {
+      const sut = makeSut()
+      const to = await makeUser()
+      const from = await makeUser()
+      const mockedChargebackTransaction = await makeTransaction()
       
+      const mockedTransaction = mockAddTransaction()
+      mockedTransaction.to = to
+      mockedTransaction.from = from
+      mockedTransaction.type = 'chargeback'
+      mockedTransaction.chargebackFrom = mockedChargebackTransaction
+      const transaction = await sut.add(mockedTransaction)
+
+      expect(transaction.amount).toBe(mockedTransaction.amount)
+      expect(transaction.description).toBe(mockedTransaction.description)
+      expect(transaction.type).toBe(mockedTransaction.type)
+      expect(transaction.to.id).toEqual(to.id)
+      expect(transaction.from.id).toEqual(from.id)
+      expect(transaction.chargebackFrom.id).toEqual(mockedChargebackTransaction.id)
+      
+      const transactionExists = await TransactionSequelize.findByPk(
+        transaction.id
+      )
+      expect(transactionExists).toBeTruthy()
+      expect(transactionExists.toJSON().id).toBe(transaction.id)
     })
   })
 })
