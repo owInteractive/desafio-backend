4c760805472998d74ee1fc65a1cba8f3b083bf53
Guilherme Teixeira Ais <guilhermeteixeiraais@gmail.com>
2022-10-20 16:40:43 -0300

    feat: serve arquivos estáticos

--- app/src/main/config/app.ts
+++ app/src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import { setupMiddlewares } from './middlewares'
 import { setupRoutes } from './routes'
+import { setupDocs } from './setup-docs'
 
 const app = express()
+setupDocs(app)
 setupMiddlewares(app)
 setupRoutes(app)
 
